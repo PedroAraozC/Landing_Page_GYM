be0f4ab105481a19b88305012d02d61e1f2d867e
Pedro Aráoz <pedroaraozc@gmail.com>
2025-01-03 13:05:09 -0300

    a

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import logo from "../../assets/icono.png";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navBarCont">
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
-      <ul className="navLinks">
+      <div className={`hamb ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`navLinks ${isOpen ? "show" : ""}`}>
         <li>
           <a href="#home">Home</a>
         </li>
